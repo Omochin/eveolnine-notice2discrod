f6e7807d63ee219544fc0a299f684559e86e2756
omochin <omochin@omochin-no-Air.mshome.net>
2017-12-12 14:56:04 +0900

    Add moons json

--- run.py
+++ run.py
@@ -1,48 +1,84 @@
-import json
+from enum import IntEnum
 import os
+import json
 import requests
 from preston.xmlapi import Preston
 
+class PosStates(IntEnum):
+    Unanchored = 0
+    Offline = 1
+    Onlining = 2
+    Reinforced = 3
+    Online = 4
+
 FUEL_IDS = [
-    4246
+    4247, # Helium Fuel Block
+    4246, # Hydrogen Fuel Block
+    4051, # Nitrogen Fuel Block
+    4312, # Oxygen Fuel Block
 ]
+
+MINIMUM_ORDER_QUANTITY = int(os.getenv('MINIMUM_ORDER_QUANTITY', 3000))
+
+DISCORD_NOTICE_WEBHOOK_URL = os.getenv('DISCORD_NOTICE_WEBHOOK_URL', None)
+DISCORD_LOG_WEBHOOK_URL = os.getenv('DISCORD_LOG_WEBHOOK_URL', None)
+
+DISCORD_NOTICE_TEXT = os.getenv('DISCORD_NOTICE_TEXT', '{moon}: {fuel}({quantity}):thinking:')
+DISCORD_LOG_TEXT = os.getenv('DISCORD_LOG_TEXT', '{moon}: {fuel}({quantity}):thinking:')
 
 def main():
     with open('types.json', 'r') as file:
         types_dict = json.load(file)
 
+    with open('moons.json', 'r') as file:
+        moons_dict = json.load(file)
+
     preston = Preston(
-        key=os.environ['EVEONLINE_API_KEY_ID'],
-        code=os.environ['EVEONLINE_API_VCODE'],
+        key=os.environ['EVEONLINE_API_KEY'],
+        code=os.environ['EVEONLINE_API_CODE'],
     )
 
     def post(url, content):
+        if url is None:
+            return
+
         requests.post(
             url,
             data=json.dumps({'content': content}),
             headers={'content-type': 'application/json'},
         )
 
-    pos = preston.corp.StarbaseList()['rowset']
-    if pos['@name'] == 'starbases':
-        pos_id = preston.corp.StarbaseList()['rowset']['row']['@itemID']
-        fuels = preston.corp.StarbaseDetail(itemID=pos_id)['rowset']['row']
-        for fuel in fuels:
-            type_id = int(fuel['@typeID'])
-            name = types_dict[fuel['@typeID']]
-            quantity = int(fuel['@quantity'])
+    rowset = preston.corp.StarbaseList()['rowset']
+    if 'row' not in rowset:
+        text = 'you do NOT have a pos:sob:'
+        post('{moon} - {fuel}({quantity}):thinking:', text)
+        post(DISCORD_LOG_WEBHOOK_URL, text)
 
-            if type_id in FUEL_IDS:
-                if 'DISCORD_TEST_WEBHOOK_URL' in os.environ:
-                    post(
-                        os.environ['DISCORD_TEST_WEBHOOK_URL'],
-                        "{}({})".format(name, quantity),
-                    )
-                if quantity < int(os.getenv('MINIMUM_ORDER_QUANTITY', 2800)):
-                    post(
-                        os.environ['DISCORD_WEBHOOK_URL'],
-                        "@everyone {}({}):scream:".format(name, quantity),
-                    )
+    row = rowset['row']
+    for pos in row if isinstance(row, list) else [row]:
+        moon_name = moons_dict[pos['@moonID']]
+        pos_state = int(pos['@state'])
+        if pos_state != PosStates.Online:
+            text = '"{}" is {}:scream:'.format(moon_name, PosStates(pos_state).name)
+            post(DISCORD_NOTICE_WEBHOOK_URL, text)
+            post(DISCORD_LOG_WEBHOOK_URL, text)
+
+        fuels = preston.corp.StarbaseDetail(itemID=pos['@itemID'])['rowset']['row']
+        for fuel in fuels:
+            fuel_id = int(fuel['@typeID'])
+            fuel_name = types_dict[fuel['@typeID']]
+            fuel_quantity = fuel['@quantity']
+
+            def post_fuel(url, content):
+                content = content.replace('{moon}', moon_name)
+                content = content.replace('{fuel}', fuel_name)
+                content = content.replace('{quantity}', fuel_quantity)
+                post(url, content)
+
+            if fuel_id in FUEL_IDS:
+                post_fuel(DISCORD_LOG_WEBHOOK_URL, DISCORD_LOG_TEXT)
+                if int(fuel_quantity) < MINIMUM_ORDER_QUANTITY:
+                    post_fuel(DISCORD_NOTICE_WEBHOOK_URL, DISCORD_NOTICE_TEXT)
 
 if __name__ == '__main__':
     main()
--- scripts/yaml2json.py
+++ scripts/yaml2json.py
@@ -1,11 +1,34 @@
+import os
 import codecs
 import json
 import yaml
 
 def main():
-    with codecs.open('typeIDs.yaml', 'r', 'utf-8') as file:
-        types_yaml = yaml.load(file)
+    def load_yaml(path):
+        with codecs.open(path, 'r', 'utf-8') as file:
+            data = yaml.load(file)
+        return data
 
+    items_yaml = load_yaml(os.path.join('sde', 'bsd', 'invItems.yaml'))
+    names_yaml = load_yaml(os.path.join('sde', 'bsd', 'invNames.yaml'))
+    moon_ids = []
+    names_dict = {}
+    moons_dict = {}
+
+    for item in items_yaml:
+        if item['typeID'] == 14:
+            moon_ids.append(item['itemID'])
+
+    for name in names_yaml:
+        names_dict[name['itemID']] = name['itemName']
+
+    for moon_id in moon_ids:
+        moons_dict[moon_id] = names_dict[moon_id]
+
+    with open('moons.json', 'w') as file:
+        json.dump(moons_dict, file)
+
+    types_yaml = load_yaml(os.path.join('sde', 'fsd', 'typeIDs.yaml'))
     types_dict = {}
     for i, items in types_yaml.items():
         try:
